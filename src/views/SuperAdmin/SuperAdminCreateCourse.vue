8dfffc9a7e6d7c3044b759588e330cdb8438561c
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2025-10-31 12:33:31 +0100

    Update SuperAdminCreateCourse.vue

--- src/views/SuperAdmin/SuperAdminCreateCourse.vue
+++ src/views/SuperAdmin/SuperAdminCreateCourse.vue
@@ -20,7 +20,7 @@ import {
 import { computed, ref } from "vue";
 import SuperAdminSidebar from "./SuperAdminSidebar.vue";
 
-const currentStep = ref(4);
+const currentStep = ref(1);
 
 const basicInfoForm = ref({
   title: "Naturopathy 101: Discover Natural Healing",
