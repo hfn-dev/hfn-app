8493520d0abdb22daaca2c2220df94563a6d190d
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2025-11-01 21:51:18 +0100

    Update Events.vue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -89,7 +89,7 @@
 
 
                         
-                        <div class="flex items-center space-x-4 text-sm text-orange-500 mb-4">
+                        <div class="flex items-center space-x-4 text-sm text-orange-500 mb-4 mt-10">
                             <span class="flex items-center">
                                 <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                                 October 31, 2025
