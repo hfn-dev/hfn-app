1c32e907298449bcf6f191f573af8ceb6a9cf48d
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2025-11-14 23:49:47 +0100

    Update MyAccount.vue

--- src/views/User/MyAccount.vue
+++ src/views/User/MyAccount.vue
@@ -5,7 +5,7 @@ import { useRouter } from "vue-router";
 
 const router = useRouter();
 
-const certificateUrl = ref("/sample-certificate.png"); 
+const certificateUrl = ref("/sample-certificates.png"); 
 
 const viewCertificateInNewTab = () => {
   if (certificateUrl.value) {
