0e5cc691f265fcb3e9cb56dd100b7066588e5ed0
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2025-11-14 12:51:16 +0100

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -168,7 +168,7 @@ import reagan from "@/assets/reagan.png";
         </div>
         <div class="mb-10 lg:mb-0">
           <div
-            class="relative w-full overflow-hidden h-80 sm:h-96 md:h-[500px]"
+            class="relative w-full overflow-hidden h-80 sm:h-96 md:h-[500px] bg-[#F2F9F3]"
           >
             <img
               :src="partnership"
