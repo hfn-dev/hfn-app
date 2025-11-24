bcc02aa825ef2aca47a4075a65e3a838b67668c5
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2025-11-24 15:07:30 +0100

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -113,7 +113,7 @@ watch(
           <img
             :src="hfn_logo"
             alt="HFN Logo"
-            class="w-36 h-auto mr-3 flex-shrink-0"
+            class="w-54 h-auto mr-3 flex-shrink-0"
           />
         </RouterLink>
       </div>
