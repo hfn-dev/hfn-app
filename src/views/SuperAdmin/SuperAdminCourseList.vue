cbdca804e2b49d4786f6fb1c04487b87ac4c8782
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-14 13:26:09 +0100

    Update SuperAdminCourseList.vue

--- src/views/SuperAdmin/SuperAdminCourseList.vue
+++ src/views/SuperAdmin/SuperAdminCourseList.vue
@@ -2,7 +2,7 @@
 import courseApi from '@/api/learningModule.js';  
 import analyticsApi from '@/api/dashboard.js';    
 import SuperAdminSidebar from '@/views/SuperAdmin/SuperAdminSidebar.vue';
-import { computed, watch } from 'vue';
+import { computed, watch, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 
 import {
