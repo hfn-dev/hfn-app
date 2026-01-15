6f8644b90a36d43561b20ef07fa1dbc42bc54ec2
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-15 07:22:44 +0100

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -41,6 +41,8 @@ const dashboardLink = computed(() => {
     superadmin: "/superadmin/dashboard",
     admin: "/admin/dashboard",
     editor: "/editor/dashboard",
+    tutor: '/tutor/dashboard',
+    learner: '/learner/dashboard',
     user: "/user/dashboard",
   };
   return roleMap[userRole.toLowerCase()] || "/dashboard";
