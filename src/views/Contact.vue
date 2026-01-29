f3daf73b8ca9e2fee628699b1119b69aabfd8685
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-29 18:15:17 +0100

    Update Contact.vue

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -201,7 +201,7 @@ import globe from "@/assets/globe.png";
                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
               ></path>
             </svg>
-            <p class="text-gray-700 text-base">executivesecretary@hfnigeria.com</p>
+            <p class="text-gray-700 text-base break-all">executivesecretary@hfnigeria.com</p>
 
           </div>
 
