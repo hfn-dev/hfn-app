434fac5a5bd23669a43f1f3428604904950dd961
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-29 19:24:20 +0100

    Update memberResources.js

--- src/api/memberResources.js
+++ src/api/memberResources.js
@@ -1,7 +1,15 @@
 import api from "./axios.js";
 
 export default {
-  listResources() {
-    return api.get("/member/resources");
+  async listResources(params = {}) {
+    try {
+      const response = await api.get("/member/resources/", {
+        params,
+      });
+      return response.data;
+    } catch (error) {
+      console.error("List membership resources API error:", error);
+      throw error;
+    }
   },
 };
