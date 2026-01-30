ce42a40ff5a2b4371a097746c8853c2d13f311ee
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-30 12:50:13 +0100

    Update AdminHeader.vue

--- src/components/layout/AdminHeader.vue
+++ src/components/layout/AdminHeader.vue
@@ -171,7 +171,7 @@ onUnmounted(() => {
           <img
             :src="hfn_logo"
             alt="HFN Logo"
-            class="w-40 h-auto mr-3 flex-shrink-0"
+            class="w-30 h-auto mr-3 flex-shrink-0"
           />
         </RouterLink>
       </div>
