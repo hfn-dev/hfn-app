fb07496812dca7bb948f4f1e976ffb532d15f1f6
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-30 10:12:46 +0100

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -65,7 +65,7 @@ const navLinks = [
     
     hasDropdown: true,
     dropdownItems: [
-      { title: "Programs & Initiatives", path: "/programs" },
+      // { title: "Programs & Initiatives", path: "/programs" },
       { title: "Latest News", path: "/news" },
       { title: "Newsletters", path: "/newsletters" },
       { title: "Events", path: "/events" },
