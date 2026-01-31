41ce41550c9289924b5f521c87c963b63ceabbde
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-01-31 22:57:07 +0100

    Update NewsAdvocacy.vue

--- src/views/NewsAdvocacy.vue
+++ src/views/NewsAdvocacy.vue
@@ -174,10 +174,9 @@
 </template>
 
 <script setup>
-import latest from "@/assets/latest_news.png"; 
+ 
 import wef from "@/assets/wef.jpg";
-
-
+const latest = 'https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769896360/243A8355_r47c3t.jpg';
 const event = 'https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769715457/16268da44f149e859e5069f5fcdbf9fb_S_j9rsms.jpg';
 const group = 'https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716176/e21b2626f11dbd7c071c1bdc38aee1f6_L_y1r0t8.jpg';
 const group1 = 'https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716176/f01ce502715837920e87942116372980_M_jb2p23.jpg';
