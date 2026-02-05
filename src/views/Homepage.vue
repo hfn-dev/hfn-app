2fa0b8fa21a447b2d86619e17753cc9d39918deb
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-02-05 15:04:58 +0100

    Update Homepage.vue

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -14,8 +14,35 @@ import {
   UserGroupIcon,
 } from "@heroicons/vue/24/outline";
 
+  import wef from "@/assets/wef.jpg";
 
-const resolveImage = (item) => {
+const latest =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769896360/243A8355_r47c3t.jpg";
+const event =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769715457/16268da44f149e859e5069f5fcdbf9fb_S_j9rsms.jpg";
+const group =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716176/e21b2626f11dbd7c071c1bdc38aee1f6_L_y1r0t8.jpg";
+const group1 =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716176/f01ce502715837920e87942116372980_M_jb2p23.jpg";
+const hands =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769715465/1feebd03da9f660dfb6e3f79b696f544_L_rxf7mk.jpg";
+const hands1 =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716174/f5b95525832f3712e665bb57dba370d3_XS_yrppya.jpg";
+const hands2 =
+  "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1769716176/dc2a1ae8ac60464700aa7be25ea2c408_L_dt5us8.jpg";
+
+const imageMap = {
+  "event.png": event,
+  "group.png": group,
+  "group1.png": group1,
+  "wef.jpg": wef,
+  "hands.png": hands,
+  "hands1.png": hands1,
+  "hands2.png": hands2,
+};
+
+
+  const resolveImage = (item) => {
   if (!item) return "/images/placeholder-news.jpg";
 
   const img =
