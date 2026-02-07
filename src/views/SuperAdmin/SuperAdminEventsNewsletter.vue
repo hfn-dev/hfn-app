c76c2232bc360e9f05934f16aeef6a4a2963d56f
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-02-07 22:25:13 +0100

    Update SuperAdminEventsNewsletter.vue

--- src/views/SuperAdmin/SuperAdminEventsNewsletter.vue
+++ src/views/SuperAdmin/SuperAdminEventsNewsletter.vue
@@ -4,7 +4,7 @@ import eventsApi from "@/api/events.js";
 import SuperAdminSidebar from "@/views/SuperAdmin/SuperAdminSidebar.vue";
 import { onMounted, ref, computed } from "vue";
  import newsApi from "@/api/news";
-import { useAuth } from "@/stores/auth";
+import { useAuth } from "@/store/authStore";
 
 const events = ref([]);
 const loadingEvents = ref(false);
