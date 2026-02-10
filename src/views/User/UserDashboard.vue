38344d1e01e7baed843dd51d7e5f77f653e1b903
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-02-10 09:16:31 +0100

    Update UserDashboard.vue

--- src/views/User/UserDashboard.vue
+++ src/views/User/UserDashboard.vue
@@ -500,7 +500,7 @@ onMounted(() => {
   </h2>
 
   <div
-    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-16"
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-16"
   >
     <div
       v-for="(item, index) in newsletterPdfs"
