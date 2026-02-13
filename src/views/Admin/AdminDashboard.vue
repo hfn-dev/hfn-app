fa2abce8aeabc2b0101386f019aed5c0a38e3c01
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-02-13 15:45:32 +0100

    Update AdminDashboard.vue

--- src/views/Admin/AdminDashboard.vue
+++ src/views/Admin/AdminDashboard.vue
@@ -514,7 +514,7 @@ const closeSidebar = () => (showSidebar.value = false);
           </p>
         </div>
 
-        <div class="flex justify-between items-stretch mb-10 space-x-6">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6 mb-10">
           <div
             v-for="(stat, index) in statCards"
             :key="stat.title"
