1879c17d2880c6e11bdb8d009184eb42c47bac22
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-02-17 09:25:53 +0100

    Update Events.vue

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -101,7 +101,7 @@
             <div
               class="self-start inline-block text-[10px] font-semibold uppercase text-orange-600 bg-white rounded-full px-3 py-1 shadow-sm border border-orange-100"
             >
-              Members Only
+              Open Events
             </div>
 
             <div
