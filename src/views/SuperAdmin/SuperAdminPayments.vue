9c4ea1dce928bda39dddbc7b333dfc1ca47d26f7
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-13 14:26:27 +0100

    Update SuperAdminPayments.vue

--- src/views/SuperAdmin/SuperAdminPayments.vue
+++ src/views/SuperAdmin/SuperAdminPayments.vue
@@ -182,7 +182,7 @@ const fetchDashboardAnalytics = async () => {
   ([month, amount]) => ({
     month,
     amount,
-  })
+  }).slice(-6);
 );
     paymentTrendData.value = (revenue.payment_method_distribution || []).map(
   (item) => ({
