7be84a2cbd89e3cfb7bc28410f360f68e61b9981
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-14 06:51:19 +0100

    Create AppToast.vue

---
+++ src/components/layout/AppToast.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useToast } from "@/composables/useToast";
+
+const { toastState } = useToast();
+</script>
+
+<template>
+  <div
+    v-if="toastState.message"
+    class="fixed top-6 right-6 z-50 px-4 py-3 rounded-lg shadow-lg text-white transition"
+    :class="{
+      'bg-green-600': toastState.type === 'success',
+      'bg-red-600': toastState.type === 'error',
+      'bg-blue-600': toastState.type === 'info'
+    }"
+  >
+    {{ toastState.message }}
+  </div>
+</template>
