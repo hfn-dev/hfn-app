4d47d261caa9175fd850b26ffd1dd826a4faca08
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-18 00:55:18 +0100

    Update memberResources.js

--- src/api/memberResources.js
+++ src/api/memberResources.js
@@ -12,4 +12,13 @@ export default {
       throw error;
     }
   },
+  async getDownloadList(id) {
+    try {
+      const response = await api.get(`/forms/${id}/submissions/`);
+      return response.data;
+    } catch (error) {
+      console.error("List membership resources API error:", error);
+      throw error;
+    }
+  },
 };
