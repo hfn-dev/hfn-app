1ac3086a45f3ab1cbcc3cb0dc3ecc991f46ef928
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-20 13:41:13 +0100

    Update EditorPages.vue

--- src/views/Editor/EditorPages.vue
+++ src/views/Editor/EditorPages.vue
@@ -445,26 +445,6 @@ const toggleSecondaryContent = (itemId) => {
     activeSecondaryContent.value === itemId ? null : itemId;
 };
 
-const addNewFaq = () => {
-  const section = activePage.value.sections["section5"];
-  if (section && section.faqs) {
-    const newId = Math.max(...section.faqs.map((f) => f.id), 0) + 1;
-    section.faqs.push({
-      id: newId,
-      question: `Question ${newId}: New FAQ Question here...`,
-      response: "New answer text here...",
-    });
-  }
-  console.log("Added new FAQ item.");
-};
-
-const deleteFaq = (faqId) => {
-  const section = activePage.value.sections["section5"];
-  if (section && section.faqs) {
-    section.faqs = section.faqs.filter((faq) => faq.id !== faqId);
-    console.log(`Deleted FAQ with ID: ${faqId}`);
-  }
-};
 
 const addMonth = () => {
   currentSectionData.value.months["New Month"] = {
