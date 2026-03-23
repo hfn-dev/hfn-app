0d0c0cb48879a5a6899add74ff94df3ae91ac7db
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-23 13:12:11 +0100

    Create GetinvolvedHeroEditor.vue

---
+++ src/views/Editor/components/cms/getinvolved/GetinvolvedHeroEditor.vue
@@ -0,0 +1,52 @@
+<template>
+
+            <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+              <label class="block text-xs font-semibold uppercase text-gray-500"
+                >Title Line 1</label
+              >
+              <input
+                v-model="currentSectionData.titleLine1"
+                type="text"
+                class="w-full border-none focus:ring-0 p-0 m-0"
+              />
+            </div>
+
+            <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+              <label class="block text-xs font-semibold uppercase text-gray-500"
+                >Title Line 2</label
+              >
+              <input
+                v-model="currentSectionData.titleLine2"
+                type="text"
+                class="w-full border-none focus:ring-0 p-0 m-0"
+              />
+            </div>
+
+            <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+              <label class="block text-xs font-semibold uppercase text-gray-500"
+                >Description</label
+              >
+              <textarea
+                v-model="currentSectionData.description"
+                rows="3"
+                class="w-full text-sm border-none focus:ring-0 p-0 m-0 resize-none"
+              ></textarea>
+            </div>
+
+            <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+              <label class="block text-xs font-semibold uppercase text-gray-500"
+                >Background Color</label
+              >
+              <input
+                v-model="currentSectionData.backgroundColor"
+                type="text"
+                class="w-full border-none focus:ring-0 p-0 m-0"
+              />
+            </div>
+
+            <image-uploader
+              v-model="currentSectionData.image"
+              label="Hero Image"
+            />
+
+</template>  
