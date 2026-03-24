eda96e6da54d73dede514b7703d361edc5b55848
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-24 12:42:53 +0100

    Create ContactMapEditor.vue

---
+++ src/views/Editor/components/cms/contact/ContactMapEditor.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref, watch } from "vue";
+
+
+const getDefaultSectionData = () => ({
+  iframeUrl:
+    "",
+
+  height: 500,
+  borderWeight: 4,
+  borderColor: "border-green-200",
+});
+
+
+const currentSectionData = ref(getDefaultSectionData());
+const emit = defineEmits(["update:modelValue"]);
+
+watch(
+  currentSectionData,
+  (val) => {
+    emit("update:modelValue", val);
+  },
+  { deep: true }
+);
+</script>
+
+<template>
+          
+            <div class="space-y-6">
+              <!-- Iframe URL -->
+              <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+                <label
+                  class="block text-xs font-semibold uppercase text-gray-500"
+                >
+                  Google Maps Iframe URL
+                </label>
+                <textarea
+                  v-model="currentSectionData.iframeUrl"
+                  rows="3"
+                  class="w-full text-sm border-none focus:ring-0 p-0 m-0 resize-none"
+                />
+              </div>
+
+              <!-- Map Height -->
+              <div class="border border-gray-300 rounded-lg p-3 space-y-2">
+                <label
+                  class="block text-xs font-semibold uppercase text-gray-500"
+                >
+                  Map Height
+                </label>
+                <input
+                  v-model="currentSectionData.height"
+                  type="number"
+                  class="w-full text-sm border-none focus:ring-0 p-0 m-0"
+                />
+              </div>
+            </div>
+
+</template>
