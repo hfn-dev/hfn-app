fdffa0eb92997d94c8ce58b8c730986fc8d49813
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-03-27 12:21:18 +0100

    Update AdminEventNewsletter.vue

--- src/views/Admin/AdminEventNewsletter.vue
+++ src/views/Admin/AdminEventNewsletter.vue
@@ -331,6 +331,7 @@ const uploadForm = ref({
   title: "",
   type: "newsletter",
   description: "",
+  summary: "",
   audience: "all",
   media_type: "image",
   youtube_url: "",
@@ -422,7 +423,7 @@ const createUpload = async () => {
     const formData = new FormData();
 
     formData.append("title", uploadForm.value.title);
-    formData.append("caption", uploadForm.value.description);
+    formData.append("summary", uploadForm.value.summary);
     formData.append("audience", uploadForm.value.audience);
     formData.append("media_type", uploadForm.value.media_type);
     formData.append("type", uploadForm.value.type);
