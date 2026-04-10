223a43ca5b5f2a1088af132a0fe527f961fdb3a8
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-10 13:49:04 +0100

    Update Conference.vue

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -38,7 +38,7 @@ const logos = [
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822284/DIgitax_ocdglv_q3hy7n.png", scale: "scale-240" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822284/doroki_logo_b1zufs_zjxd21.jpg", scale: "scale-160" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822274/gabbi_Logo_yn9mo3_usuhga.png", scale: "scale-95" },
-  { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535500/IHS-Logo-Primary-Large_ia9x2r.png", scale: "scale-65" },
+  { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822279/IHS-Logo-Primary-Large_ia9x2r_dvy9gq.png", scale: "scale-65" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535499/Instrat_Global_tmpqt0.jpg", scale: "scale-105" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535481/Iwosan_Logos_2_c4ygwa.png", scale: "scale-135" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535477/PUREHEALTH_1_fjapth.png", scale: "scale-110" },
