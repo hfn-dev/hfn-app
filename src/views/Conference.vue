d14b9d3e5fd20451d8c35216dd99124c181917ad
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-10 13:42:59 +0100

    Update Conference.vue

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -36,7 +36,7 @@ const logos = [
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822284/Codix_Logo_mxkelg_yvvkgq.png", scale: "scale-120" },
 { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822283/Copy_of_myitura_tygw31_mpmkwa.png", scale: "scale-70" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822284/DIgitax_ocdglv_q3hy7n.png", scale: "scale-240" },
-  { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535502/doroki_logo_b1zufs.jpg", scale: "scale-160" },
+  { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822284/doroki_logo_b1zufs_zjxd21.jpg", scale: "scale-160" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822274/gabbi_Logo_yn9mo3_usuhga.png", scale: "scale-95" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535500/IHS-Logo-Primary-Large_ia9x2r.png", scale: "scale-65" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535499/Instrat_Global_tmpqt0.jpg", scale: "scale-105" },
