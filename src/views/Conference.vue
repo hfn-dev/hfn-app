a72d300d234f43af3e29554f2e3c5806283f94bd
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-10 16:06:28 +0100

    Update Conference.vue

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -23,7 +23,7 @@ const logos = [
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1771061210/jnci-logo_x2oau2.png", scale: "scale-100" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1771061210/HRPayHub_LOGO_wpgja7.png", scale: "scale-165" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822282/cedarcrest-hospitals-logo-full-color-rgb-1-300x201_plcdzx_huzoho.png", scale: "scale-105" },
-  { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1771066720/emzor-B-OOqpe8_bat5fl.png", scale: "scale-155" },
+  { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822275/emzor-B-OOqpe8_bat5fl_wzfvlz.png", scale: "scale-155" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1771066719/hba-africa-BXqx92lp_j2kbgl.png", scale: "scale-135" },
   { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822282/bridge-BbygTYH0_g9x0si_dnqwij.png", scale: "scale-110" },
 
