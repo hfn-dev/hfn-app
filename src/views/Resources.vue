8d190131e2c84391613d7fc804e2768ea7081751
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-10 15:57:54 +0100

    Update Resources.vue

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -438,7 +438,7 @@ const dummyPublications = [
     description:
       "The Healthcare Federation of Nigeria (HFN), in collaboration with the West Africa Private Healthcare Federation (FOASPS), the Presidential Initiative for Unlocking the Healthcare Value Chain (PVAC), the African Union Development Agency (AUDA-NEPAD), and the World Bank, convened a High-Level Roundtable on Local Manufacturing of Medicines in Nigeria on Wednesday, October 22, 2025, in Abuja",
     pdfUrl:
-      "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772817006/Nigeria_Private_Health_Sector_Market_Outlook_2026_c4ql1j.pdf",
+      "https://res.cloudinary.com/dmbckag42/image/upload/v1775821992/HFN_2025_Year_in_Review__eicos6_mzcspn.pdf",
   },
   {
     id: 4,
