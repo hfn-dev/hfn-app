65ef73219fa4a9a3991192db1cdf2bd603f97a50
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-10 13:16:04 +0100

    Update Conference.vue

--- src/views/Conference.vue
+++ src/views/Conference.vue
@@ -52,7 +52,7 @@ const logos = [
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535474/Mdoc_Logo_wwu5jj.png", scale: "scale-160" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772535474/sfhaccess_logo_ouxu9n.jpg", scale: "scale-150" },
 
-  { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772552251/AMCE_LOGO_ut8leh.jpg", scale: "scale-150" },
+  { src: "https://res.cloudinary.com/dmbckag42/image/upload/v1775822280/AMCE_LOGO_ut8leh_nuae8b.jpg", scale: "scale-150" },
   { src: "https://res.cloudinary.com/pou7gd5q41xc/image/upload/v1772552251/Bristole_Scientific_pydixu.png", scale: "scale-180" },
 
 ]
