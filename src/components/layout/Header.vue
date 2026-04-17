6c7722b67d12b8bb3c632d999c4435736be6c2b9
seyigeorge <54175924+seyigeorge@users.noreply.github.com>
2026-04-17 13:31:41 +0100

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -87,7 +87,7 @@ const navLinks = [
       { title: "Newsletters", path: "/resources" },
       { title: "Events", path: "/events" },
       { title: "Gallery", path: "/gallery" },
-      { title: "2026 Annual Conference", path: "/conference" },
+      { title: "2026 Annual Conference", path: "#" },
     ],
   },
   {
